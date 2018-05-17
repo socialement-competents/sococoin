711658ca5c2acdeb237dbd8a39e0b411c6dc0dc3
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-17 11:22:28 +0200

    Init the SocoCoin and allow minting to reward users

--- SocoCoin.sol
+++ SocoCoin.sol
@@ -3,5 +3,20 @@ pragma solidity ^0.4.21;
 import "./OwnableERC20.sol";
 
 contract SocoCoin is OwnableTokenERC20 {
-  
+    constructor () OwnableTokenERC20(
+        0,
+        "SocoCoin",
+        "SOCO"
+    ) public {}
+
+    // Mint tokens
+    function reward(address _target, uint256 amount) public onlyOwner {
+        // Mint some tokens out of nowhere
+        balanceOf[_target] += amount;
+        // Update the total supply
+        totalSupply += amount;
+        // Indicate that the owner got [amount] minted tokens and sent it to [_target]
+        emit Transfer(0, owner, amount);
+        emit Transfer(owner, _target, amount);
+    }
 }
