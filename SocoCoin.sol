a76082b280e3e3c619b09ac41dc319368f10eb36
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-17 20:03:48 +0200

    Allow any one to claim rewards - not sure how this will play out

--- SocoCoin.sol
+++ SocoCoin.sol
@@ -11,12 +11,19 @@ contract SocoCoin is OwnableTokenERC20 {
 
     // Mint tokens
     function reward(address _target, uint256 amount) public onlyOwner {
-        // Mint some tokens out of nowhere
+        // Mint some tokens out of thin air
         balanceOf[_target] += amount;
         // Update the total supply
         totalSupply += amount;
         // Indicate that the owner got [amount] minted tokens and sent it to [_target]
         emit Transfer(0, owner, amount);
         emit Transfer(owner, _target, amount);
+    }
+
+    function claimReward(uint256 amount) public {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(0, owner, amount);
+        emit Transfer(owner, msg.sender, amount);
     }
 }
